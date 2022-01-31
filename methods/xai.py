7d34a45863df1fa5789ba8d3d31bbc93677bd195
Alexandre <andralex8@gmail.com>
2022-01-31 14:23:17 +0100

    xai: Enable Rise

--- methods/xai.py
+++ methods/xai.py
@@ -1,7 +1,10 @@
 import captum
+import torch
 from captum.attr import NoiseTunnel
 from captum.attr import IntegratedGradients
 from captum.attr import LayerGradCam
+
+from RISE.explanations import RISE
 
 class SmoothGrad(NoiseTunnel):
     def __init__(self, model):
@@ -14,6 +17,17 @@ class SmoothGrad(NoiseTunnel):
 class GradCAM(LayerGradCam):
     def __init__(self, model):
         super().__init__(model, model.layer4)
+
+class Rise:
+    def __init__(self, model, input_size=224, batch_size=16):
+        params = methods_dict['rise']['params_attr']
+        self.rise = RISE(model, (input_size, input_size), batch_size)
+        self.rise.generate_masks(N=params['n_masks'], s=8, p1=0.1)
+        self.input_size = input_size
+
+    def attribute(self, inputs, target=None):
+        with torch.no_grad():
+            return self.rise(inputs)[target].view((1, 1, self.input_size, self.input_size))
 
 
 methods_dict = {
@@ -28,7 +42,13 @@ methods_dict = {
     },
     'gradcam': {
         'class_fn': GradCAM,
-    }
+    },
+    'rise': {
+        'class_fn': Rise,
+        'params_attr': {
+            'n_masks': 4000,
+        }
+    },
 }
 
 
